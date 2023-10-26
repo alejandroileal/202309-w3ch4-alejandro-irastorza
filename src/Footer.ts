a33b16bb218499cac4281c6350225db644bf8998
alejandroileal <aleal1202@outlook.com>
2023-10-27 00:14:42 +0200

    Add Footer files

---
+++ src/Footer.ts
@@ -0,0 +1,13 @@
+import { Component } from './Component';
+
+export class Footer extends Component {
+  constructor(selector: string) {
+    super(selector);
+    this.template;
+    this.render();
+  }
+
+  createTemplate() {
+    return;
+  }
+}
